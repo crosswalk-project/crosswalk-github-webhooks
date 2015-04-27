c2e115a844e91f60f30e7ce32116dda84dec0ffa
Raphael Kubo da Costa <raphael.kubo.da.costa@intel.com>
2015-04-27 18:12:15 +0300

    github_webhooks: Add tests for the GitHub payload signature checks.

--- github_webhooks/tests.py
+++ github_webhooks/tests.py
@@ -2,10 +2,13 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+import hashlib
+import json
 import mock
 
 from django.core.urlresolvers import reverse
 from django.test import TestCase
+from django.test.client import Client
 
 from github_webhooks.test.utils import GitHubEventClient
 
@@ -14,6 +17,18 @@ class GitHubWebhooksTestCase(TestCase):
     def setUp(self):
         self.client = GitHubEventClient()
         self.url = reverse('github_webhooks.views.dispatch_pull_request')
+
+    def test_no_github_signature(self):
+        payload = json.dumps({'payload': {'a': 'b'}})
+        response = Client().post(self.url, payload, 'application/json')
+        self.assertEqual(response.status_code, 404)
+
+    def test_wrong_github_signature(self):
+        payload = json.dumps({'payload': {'a': 'b'}})
+        signature = 'sha1=%s' % hashlib.sha1('wrong').hexdigest()
+        response = Client().post(self.url, payload, 'application/json',
+                                 HTTP_X_HUB_SIGNATURE=signature)
+        self.assertEqual(response.status_code, 404)
 
     @mock.patch('github_webhooks.signals.pull_request_changed.send')
     def test_ping_payload(self, mock_pull_request_changed):
